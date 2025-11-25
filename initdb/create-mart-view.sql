349c4cd306689c675e8e49cdd8f1a7ae3d06464b
Nguyễn Hoài Khương <22130128@st.hcmuaf.edu.vn>
2025-11-26 00:16:29 +0700

    Delete initdb directory

--- initdb/create-mart-view.sql
+++
@@ -1,7 +0,0 @@
-\connect datawarehouse
-CREATE SCHEMA IF NOT EXISTS mart;
-CREATE MATERIALIZED VIEW IF NOT EXISTS mart.dm_daily_price AS
-SELECT f.date, p.name, COUNT(*) as cnt, AVG(f.price) as avg_price, MIN(f.price) as min_price, MAX(f.price) as max_price
-FROM dw.fact_price_snapshot f
-JOIN dw.dim_product p ON f.product_id = p.product_id
-GROUP BY f.date, p.name;
